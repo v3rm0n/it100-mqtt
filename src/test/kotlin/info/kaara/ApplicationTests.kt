05b31dbc56ac796a219f508ceff2e4a143d0027e
Maido Käära <maido@transferwise.com>
2016-03-05 21:12:41 +0200

    Fixed build failing

--- src/test/kotlin/info/kaara/ApplicationTests.kt
+++ src/test/kotlin/info/kaara/ApplicationTests.kt
@@ -3,17 +3,25 @@ package info.kaara
 import info.kaara.it100.Application
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.context.web.WebAppConfiguration
 
 @RunWith(SpringJUnit4ClassRunner::class)
 @SpringApplicationConfiguration(classes = arrayOf(Application::class))
 @WebAppConfiguration
+@ActiveProfiles("mock")
 class ApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Autowired
+    lateinit var applicationContext: ConfigurableApplicationContext
+
+    @Test
+    fun contextLoads() {
+        applicationContext.close()
+    }
 
 }
